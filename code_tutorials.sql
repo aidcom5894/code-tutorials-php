a26ef3581661d10e5640cbe8d2ca7167d793cf38
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-07-30 00:43:30 +0530

    updated new sql file

--- code_tutorials.sql
+++ code_tutorials.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 29, 2023 at 10:12 PM
+-- Generation Time: Jul 30, 2023 at 12:41 AM
 -- Server version: 8.0.33-0ubuntu0.22.04.4
 -- PHP Version: 7.4.33
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `recovery_db`
+--
+
+CREATE TABLE `recovery_db` (
+  `id` int NOT NULL,
+  `username` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `useremail` varchar(350) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `usercontact` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `onboarding_date` varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
+  `termination_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `recovery_date` varchar(120) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `recovery_db`
+--
+
+INSERT INTO `recovery_db` (`id`, `username`, `useremail`, `usercontact`, `onboarding_date`, `termination_date`, `recovery_date`) VALUES
+(1, 'sdfsdf', 'asd@gmail.com', '23423423', '2023-07-30 00:15:15', '2023-07-30 00:15:19', 'To be Updated on Recovery'),
+(2, 'sgdfg', 'asdk@gmail.com', '11231654', '2023-07-30 00:17:10', '2023-07-30 00:17:36', 'To be Updated on Recovery');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userdetails`
 --
 
@@ -36,22 +60,14 @@ CREATE TABLE `userdetails` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `userdetails`
---
-
-INSERT INTO `userdetails` (`id`, `username`, `useremail`, `usercontact`, `registration_date`) VALUES
-(1, 'Vivek Robin Kujur', 'vrobinkujur@gmail.com', '9264453821', '2023-07-29 17:11:42'),
-(2, 'Syed Hamza Roshan', 'syedhamzaroshan22@gmail.com', '9955048876', '2023-07-29 17:13:50'),
-(3, 'Shahil Eqbal', 'shahileqbal22@gmail.com', '9874563210', '2023-07-29 17:14:29'),
-(4, 'Shweta Devi', 'shweta@gmail.com', '9632587410', '2023-07-29 17:14:47'),
-(5, 'Vivek Sharma', 'sharmavivek22@gmail.com', '7410258963', '2023-07-29 17:15:18'),
-(6, 'Shilpi', 'shilpi@gmail.com', '9856237401', '2023-07-29 17:16:17'),
-(7, 'Aman Kumar', 'amankumar16@gmail.com', '9569568452', '2023-07-29 17:25:09'),
-(8, 'Prince Kumar Mishra', 'princemishra22@gmail.com', '7854896321', '2023-07-29 17:26:59');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `recovery_db`
+--
+ALTER TABLE `recovery_db`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `userdetails`
@@ -64,10 +80,16 @@ ALTER TABLE `userdetails`
 --
 
 --
+-- AUTO_INCREMENT for table `recovery_db`
+--
+ALTER TABLE `recovery_db`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `userdetails`
 --
 ALTER TABLE `userdetails`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
