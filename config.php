6518b5b2f1e5fbd7e6f885a7446b280e1971eb1f
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-07-30 08:10:49 +0530

    updated config

--- config.php
+++ config.php
@@ -23,45 +23,6 @@
 
 <body>
 
-
-<nav class="navbar navbar-expand-lg navbar-info bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <button class="btn btn-outline-success me-2" type="submit">Upgrade Account</button>
-        <button class="btn btn-outline-success me-2" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
-
 <?php 
 
 $hostname = 'localhost';
@@ -112,7 +73,7 @@ $config = mysqli_connect($hostname,$username,$password,$dbname);
 			echo "<div class='alert alert-success d-flex align-items-center' role='alert'>";
 			echo " <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'><use xlink:href='#check-circle-fill'/></svg>";
 			echo "<div>";
-			echo "Welcome,"."<strong>&nbsp;".gethostname()."</strong>,&nbsp;&nbsp;". "Database Connection Established Successfully";
+			echo "Welcome,"."<strong>&nbsp;".gethostbyaddr($_SERVER['REMOTE_ADDR'])."</strong>,&nbsp;&nbsp;". "Database Connection Established Successfully";
 			echo "</div>";
 			echo "</div>";
 
